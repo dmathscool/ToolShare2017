e83f262d54e2eb0b8953cd3da8dbf67f5e688f23
gstilwell <gstilwell@users.noreply.github.com>
2017-02-07 23:39:54 -0500

    add borrow page

---
+++ js/filters.js
@@ -0,0 +1,35 @@
+function populateToolnameDropdown() {
+    $(".filtertoolname").append('<option value="name1">Name1</option>');
+    $(".filtertoolname").append('<option value="name2">Name2</option>');
+    $(".filtertoolname").append('<option value="name3">Name3</option>');
+    $(".filtertoolname").append('<option value="name4">Name4</option>');
+    $(".filtertoolname").append('<option value="name5">Name5</option>');
+}
+
+function populateTooltypeDropdown() {
+    $(".filtertooltype").append('<option value="type1">Type1</option>');
+    $(".filtertooltype").append('<option value="type2">Type2</option>');
+    $(".filtertooltype").append('<option value="type3">Type3</option>');
+    $(".filtertooltype").append('<option value="type4">Type4</option>');
+    $(".filtertooltype").append('<option value="type5">Type5</option>');
+}
+
+function populateBrandDropdown() {
+    $(".filterbrand").append('<option value="brand1">Brand1</option>');
+    $(".filterbrand").append('<option value="brand2">Brand2</option>');
+    $(".filterbrand").append('<option value="brand3">Brand3</option>');
+    $(".filterbrand").append('<option value="brand4">Brand4</option>');
+    $(".filterbrand").append('<option value="brand5">Brand5</option>');
+}
+
+function populateConditionDropdown() {
+    $(".filtercondition").append('<option value="condition1">Condition1</option>');
+    $(".filtercondition").append('<option value="condition2">Condition2</option>');
+    $(".filtercondition").append('<option value="condition3">Condition3</option>');
+    $(".filtercondition").append('<option value="condition4">Condition4</option>');
+    $(".filtercondition").append('<option value="condition5">Condition5</option>');
+}
+
+function filterStuff() {
+    
+}

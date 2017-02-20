518d1bb08dc7ccaa3ceb701ac90bef5cdb8cf0e5
gstilwell <gstilwell@users.noreply.github.com>
2017-02-20 16:26:08 -0500

    userinfo page now populates from database

---
+++ code/DatabaseRelated/getuserinfo.php
@@ -0,0 +1,22 @@
+<?php
+$conn=mysqli_connect("localhost","magic947_arthur","sweng987$$$","magic947_toolshare");
+
+$name=$_POST['name1'];
+
+$result=mysqli_query($conn,"SELECT * FROM RegUsers WHERE
+                     username='$name'");
+$numRows= $result->num_rows;
+
+$to_encode = array();
+  while( $row = $result->fetch_assoc() ) {
+  $to_encode[] = $row;
+}
+
+$jsonString = json_encode($to_encode);
+
+if (($jsonString)==FALSE) {
+  echo '';
+} else {
+  echo $jsonString;
+}
+?>

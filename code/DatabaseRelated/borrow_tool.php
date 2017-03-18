fd212f136bc78eb781f3bca654f7f611274fbfcc
akurlej <akurlej@gmail.com>
2017-03-18 14:58:55 -0400

    fleshed out borrow tool function, need to test it

--- code/DatabaseRelated/borrow_tool.php
+++ code/DatabaseRelated/borrow_tool.php
@@ -6,20 +6,31 @@ $conn=mysqli_connect("localhost","magic947_arthur","sweng987$$$","magic947_tools
 
 
 $borrowingUser=$_POST['borrowingUser'];
-$toolId=$_POST['toolId'];
+$toolId=intval($_POST['toolId']);
 $newState=$_POST['newToolState']
 
-if ($newState == 4){
+if ($newState == "Available"){
   //this represents going from Returned -> Available, here we increment the state
   //and remove the currentUser (set to null)
+  $result = mysqli_query($conn,
+                        "UPDATE Tools SET RegUsers_CurrentUser = NULL,
+                        ToolState = 1
+                        WHERE idTool = $toolId");
 }
-elseif ($newState == 3){
+elseif ($newState == "Returned"){
   //this represents going from On Loan -> Returned, basically just increment the state
+  $result = mysqli_query($conn,
+                        "UPDATE Tools SET ToolState = 4
+                        WHERE idTool = $toolId");
 }
-elseif ($newState == 2){
+elseif ($newState == "On Loan"){
     //this represents going from Loan Requested -> on loan, basically just increment the state
+    $result = mysqli_query($conn,
+                          "UPDATE Tools SET ToolState = 3
+                          WHERE idTool = $toolId");
 }
-elseif ($newState ==1) {
+elseif ($newState =="Loan Requested") {
+  // GO FROM AVAILABLE -> LOAN REQUESTED
   // grab the primary id for this username
   $result=mysqli_query($conn,"SELECT idRegisteredUsers FROM RegUsers WHERE username = '$borrowingUser'");
   $data = mysqli_num_rows($result);
@@ -28,7 +39,19 @@ elseif ($newState ==1) {
     //here''s what we gotta do
     //Change the CurrentUserField to the id for the borrowingUser
     //increment to the newState and we're done
+    $id = array();
+    $id = $result->fetch_assoc(); $id=intval($id['idRegisteredUsers']);
+    $result=mysqli_query($conn,
+                          "UPDATE Tools SET RegUsers_CurrentUser = $id,
+                          ToolState = 2 WHERE idTool = $toolId");
   }
+}
+
+if ($result) {
+  echo "SUCCED";
+}
+else {
+  echo "FAIL";
 }
 
 mysqli_close($conn);
